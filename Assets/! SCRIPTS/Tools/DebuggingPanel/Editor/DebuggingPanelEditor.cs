18b56cbebf862064ba0f6e0c29766df20fb020a0
Alopar <nuper@mail.ru>
2023-09-21 22:17:54 +0300

    change: debugging panel

---
+++ Assets/! SCRIPTS/Tools/DebuggingPanel/Editor/DebuggingPanelEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine.UIElements;
+using UnityEditor.UIElements;
+
+namespace Gameplay
+{
+    [CustomEditor(typeof(DebuggingPanel))]
+    public class DebuggingPanelEditor : Editor
+    {
+        public VisualTreeAsset uxml;
+
+        public override VisualElement CreateInspectorGUI()
+        {
+            var root = new VisualElement();
+            uxml.CloneTree(root);
+
+            var foldout = new Foldout()
+            {
+                text = "Base inspector",
+                viewDataKey = "DebuggingPanel-BaseInspector-Foldout",
+            };
+
+            InspectorElement.FillDefaultInspector(foldout, serializedObject, this);
+            root.Add(foldout);
+
+            return root;
+        }
+    }
+}
